5f13d5caa5ef1f9194cd2279f369636a9c8b943e
Jonathan <110411712+Jony1110@users.noreply.github.com>
2024-10-24 00:16:43 -0400

    actualizacion de CreateProfesorDto

--- src/EduTrack/EduTrack.API/Dtos/CreateProfesorDto.cs
+++ src/EduTrack/EduTrack.API/Dtos/CreateProfesorDto.cs
@@ -3,15 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EduTrack.API.Dtos
 {
-    public class CreateProfesorDto : ProfesorDto
+    public class CreateProfesorDto
     {
-        //public int Id { get; set; }
-        //public string Name { get; set; }
-        //public string Lastname { get; set; }
-        //public string Email { get; set; }
-        //public string Phone { get; set; }
-        //public char Gender { get; set; }
-        //public string Birthdate { get; set; }
-        //public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public char Gender { get; set; }
+        public string Birthdate { get; set; }
+        public bool IsActive { get; set; }
     }
 }
