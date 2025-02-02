3980cb0900294c8ae0ca023e221b7023875b920d
Jonathan <110411712+Jony1110@users.noreply.github.com>
2025-02-02 19:42:28 -0400

    Inicio de sesion

---
+++ src/EduTrack/EduTrack.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduTrack.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public UsuarioController()
+        {
+
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["UsuarioId"] = id; // ID del usuario
+            return View();
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            ViewData["UsuarioId"] = id; // ID del usuario
+            return View();
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            ViewData["UsuarioId"] = id; // ID del usuario
+            return View();
+        }
+    }
+}
---
+++ src/EduTrack/EduTrack.Web/Models/ViewModels/CreateUsuarioViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduTrack.Domain.ViewModels
+{
+    public class CreateUsuarioViewModel
+    {
+        public int Id { get; set; } // Identificador del usuario
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } // Nombre del usuario
+
+        [MaxLength(20)]
+        public string Matricula { get; set; } // Matrícula, opcional para algunos roles
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Correo { get; set; } // Correo electrónico
+
+        [MaxLength(20)]
+        public string Phone { get; set; } // Teléfono
+
+        [Required]
+        [MaxLength(1)]
+        public string Gender { get; set; } // Género
+
+        [MaxLength(15)]
+        public string Birthdate { get; set; } // Fecha de nacimiento
+
+        [Required]
+        [MaxLength(50)]
+        public string Rol { get; set; } // Rol del usuario
+
+        [MaxLength(50)]
+        public string FechaRegistro { get; set; } // Fecha de registro
+
+        public bool IsActive { get; set; } // Estado activo o inactivo
+    }
+}
